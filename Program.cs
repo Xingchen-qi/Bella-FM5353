33781f595f1302b2e42dd54b0d8311089c56189b
Xingchen-qi <qi000073@umn.edu>
2025-09-23 23:41:05 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 class Program
 {
@@ -7,25 +7,25 @@ class Program
         Console.WriteLine("Monte Carlo Option Pricing Simulator");
 
         Console.Write("Initial stock price: ");
-        double s0 = double.Parse(Console.ReadLine());
+        double S0 = double.Parse(Console.ReadLine());
 
         Console.Write("Strike price: ");
-        double k = double.Parse(Console.ReadLine());
+        double K = double.Parse(Console.ReadLine());
 
         Console.Write("Risk-free rate: ");
         double r = double.Parse(Console.ReadLine());
 
         Console.Write("Volatility: ");
-        double v = double.Parse(Console.ReadLine());
+        double sigma = double.Parse(Console.ReadLine());
 
         Console.Write("Time to maturity: ");
-        double t = double.Parse(Console.ReadLine());
+        double T = double.Parse(Console.ReadLine());
 
         Console.Write("Number of steps: ");
-        int steps = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
 
         Console.Write("Number of simulations: ");
-        int sims = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
         
         Console.Write("Do you want to use Antithetic Variance Reduction? ");
         bool useAntithetic=Console.ReadLine().ToLower() == "yes";
@@ -33,72 +33,67 @@ class Program
         Console.Write("Do you want to use Control Variate?:");
         bool useControlVariate = Console.ReadLine().ToLower() == "yes";
 
-        double[] call = new double[sims];
-        double[] put= new double[sims];
-        double[] stValues = new double[sims];
-        double[] controlVars = new double[sims];
+        double dt = T / n;
+        double a1 = (r - 0.5 * sigma * sigma) * dt; 
+        double a2 = sigma * Math.Sqrt(dt); 
+        double stt = Math.Exp(r * dt); 
         Random rand = new Random();
-        for (int i = 0; i < sims; i++)
+        double sumPayoff = 0.0;
+
+        for (int j = 0; j < m; j++)
         {
-            double st = s0;
-            double stAnti = s0;
-            double dt = t / steps;
+            double s = S0;
+            double sAnti = S0;
+            double cv = 0.0;
+            double cvAnti = 0.0;
 
-            for (int j = 0; j< steps; j++)
+            for (int i = 1; i <= n; i++)
             {
+                double tau = (i - 1) * dt;
+                double delta = BlackScholesDelta(s, tau, K, T, sigma, r);
                 double z = BoxMuller(rand);
-                double drift = (r-0.5*v*v)*dt;
-                double vol  = v * Math.Sqrt(dt);
+                double st = s * Math.Exp(a1 + a2 * z);
+                cv += delta * (st - s * stt);
+                s = st;
 
-                st *= Math.Exp(drift + vol * z);
                 if (useAntithetic)
-                    stAnti *= Math.Exp(drift - vol * z);
+                {
+                    double deltaAnti = BlackScholesDelta(sAnti, tau, K, T, sigma, r);
+                    double stback = sAnti * Math.Exp(a1 - a2 * z);
+                    cvAnti += deltaAnti * (stback - sAnti * stt);
+                    sAnti = stback;
+                }
             }
-            double payoffCall = Math.Max(st - k, 0);
-            double payoffPut = Math.Max(k - st, 0);
+            double CT = Math.Max(s - K, 0.0);
+            if (useControlVariate)
+                CT -= cv;
 
             if (useAntithetic)
             {
-                payoffCall = (payoffCall + Math.Max(stAnti - k, 0)) / 2.0;
-                payoffPut = (payoffPut + Math.Max(k - stAnti, 0)) / 2.0;
+                double CTanti = Math.Max(sAnti - K, 0.0);
+                if (useControlVariate)
+                    CTanti -= cvAnti;
+
+                CT = 0.5 * (CT + CTanti);
             }
 
-            call[i] = payoffCall;
-            put[i] = payoffPut;
-            stValues[i] = st;
+            sumPayoff += CT;
         }
-        if (useControlVariate)
-        {
-            double d1 = (Math.Log(s0 / k) + (r + 0.5 * v * v) * t) / (v * Math.Sqrt(t));
-            double delta = CumDensity(d1);
-            double expectedST = s0 * Math.Exp(r * t);
-            for (int i = 0; i < sims; i++)
-            {
-                controlVars[i] = delta * (stValues[i] - expectedST);
-            }
-
-            double meanCall = Avg(call);
-            double meanControl = Avg(controlVars);
-
-            double beta = Cov(call, controlVars, meanCall, meanControl) / Var(controlVars, meanControl);
-
-            for (int i = 0; i < sims; i++)
-            {
-                call[i] = call[i] - beta * (controlVars[i] - 0.0);
-            }
-        }
-
-        double call_avg = Avg(call);
-        double put_avg = Avg(put);
-        double call_se = StdErr(call, call_avg, sims);
-        double put_se= StdErr(put, put_avg, sims);
-        double call_price=Math.Exp(-r * t) * call_avg;
-        double put_price= Math.Exp(-r * t) * put_avg;
-
-        Console.WriteLine($"\nCall price: {call_price:F4} | StdErr: {call_se:F4}");
-        Console.WriteLine($"Put  price: {put_price:F4} | StdErr: {put_se:F4}");
+        double callPrice = Math.Exp(-r * T) * (sumPayoff / m);
+        Console.WriteLine($"Call Price: {callPrice:F4}");
     }
-    
+
+    static double BlackScholesDelta(double S, double t, double K, double T,
+                                    double sigma, double r)
+    {
+        double tau = T - t;
+        if (tau <= 0) return (S > K ? 1.0 : 0.0);
+
+        double d1 = (Math.Log(S / K) + (r + 0.5 * sigma * sigma) * tau)
+                    / (sigma * Math.Sqrt(tau));
+        return CumDensity(d1);
+    }
+
     static double BoxMuller(Random rand)
     {
         double u1 = 1.0 - rand.NextDouble();
@@ -106,35 +101,7 @@ class Program
         return Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Cos(2.0 * Math.PI * u2);
     }
 
-    static double Avg(double[] arr)
-    {
-        double sum = 0;
-        foreach (var x in arr) sum += x;
-        return sum / arr.Length;
-    }
 
-    static double StdErr(double[] arr, double mean, int n)
-    {
-        double sum = 0;
-        foreach (var x in arr) sum += (x - mean) * (x - mean);
-        double std = Math.Sqrt(sum / (n - 1));
-        return std / Math.Sqrt(n);
-    }
-
-    static double Cov(double[] x, double[]y, double meanX, double meanY)
-    {
-        double sum = 0;
-        for (int i = 0; i<x.Length; i++)
-            sum += (x[i] - meanX) * (y[i] - meanY);
-        return sum / (x.Length - 1);
-    }
-    static double Var(double[] x, double mean )
-    {
-        double sum = 0;
-        for ( int i = 0; i < x.Length; i++)
-            sum += (x[i] - mean) *(x[i] - mean);
-            return sum / (x.Length - 1);
-    }
     static double CumDensity(double z)
     {
         double p = 0.3275911;
@@ -152,4 +119,7 @@ class Program
         return 0.5 * (1.0 + sign * erf);
     }
 }
+
+
+
 
